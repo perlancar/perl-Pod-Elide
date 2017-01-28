db40a566bf0d7bdb3637311a874c8c371bb66153
perlancar (on pc-home) <perlancar@gmail.com>
2017-01-28 19:11:48 +0700

    Initial commit

---
+++ lib/Pod/Elide.pm
@@ -0,0 +1,146 @@
+package Pod::Elide;
+
+# DATE
+# VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+
+use Exporter;
+our @ISA = qw(Exporter);
+our @EXPORT_OK = qw(elide);
+
+sub _markup {
+    my ($cmd, $rest) = @_;
+
+    my $prio = 10;
+    if ($cmd =~ /\Ahead(\d+)\z/) {
+        $prio = $1;
+    }
+
+    "<elspan prio=$prio>=$cmd$rest</elspan>";
+}
+
+sub elide {
+    require String::Elide::Lines;
+
+    my ($str, $len, $opts) = @_;
+
+    $opts //= {};
+
+    $str =~ s/^=(\w+)(.*\R(?:\R|\z))/_markup($1, $2)/egm;
+    #print $str;
+    String::Elide::Lines::elide($str, $len, {%$opts, default_prio=>999});
+}
+
+1;
+# ABSTRACT: Elide POD lines from a string, with options
+
+=head1 SYNOPSIS
+
+ use Pod::Elide qw(elide);
+ print elide(<<EOP, 20);
+ =head1 NAME
+
+ Foo - Do something fooish
+
+ =head1 VERSION
+
+ 1.23
+
+ =head1 SYNOPSIS
+
+  blah blah
+  blah blah
+  blah blah
+
+ =head1 DESCRIPTION
+
+ Some description some description some description. Some description some
+ description some description some description. Some description some
+ description some description. Some description some description some
+ description some description. Some description some description some
+ description. Some description some description some description some
+ description.
+
+ =head1 FUNCTIONS
+
+ =head2 func1
+
+ Blah blah blah
+ Blah blah blah
+
+ =head2 func2
+
+ Blah blah blah
+ Blah blah blah
+
+ =head1 SEE ALSO
+
+ L<Bar>
+
+ =cut
+ EOP
+
+The output is something like:
+
+ =head1 NAME
+
+ =head1 VERSION
+
+ =head1 SYNOPSIS
+
+ =head1 DESCRIPTION
+
+ Some description some description some description. Some description some
+ description some description some description. Some description some
+ ..
+ =head1 FUNCTIONS
+
+ =head2 func1
+
+ =head2 func2
+
+ =head1 SEE ALSO
+
+ =cut
+
+
+=head1 DESCRIPTION
+
+String::Elide::Lines is based on L<String::Elide::Parts> but works on a per-line
+basis.
+
+
+=head1 FUNCTIONS
+
+=head2 elide($str, $len[, \%opts]) => str
+
+Elide lines from a string if the string contains more than C<$len> lines.
+
+String can be marked with C<< <elspan prio=N truncate=T marker=M>...</elspan> >>
+so there can be multiple parts with different priorities and truncate direction.
+The default priority is 1. You can mark less important lines with higher
+priority to let it be elided first. The markup will be removed from the string
+before eliding.
+
+Known options:
+
+=over
+
+=item * marker => str (default: '..')
+
+=item * truncate => 'top'|'middle'|'bottom'|'ends' (default: 'bottom')
+
+=back
+
+
+=head1 SEE ALSO
+
+L<String::Elide::Parts> is the basis of this module but works on a per-character
+basis. See that module's SEE ALSO for list of other string eliding modules.
+
+L<Pod::Elide> uses this module.
+
+=cut
